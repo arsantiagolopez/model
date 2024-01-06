2e310125639ff41b1844a9db1689101fb752ab2b
Alexander Santiago <arsantiagolopez@gmail.com>
2024-01-06 10:23:59 -0600

    Failed attempt to host any scraping on vercel

--- src/puppeteer/index.ts
+++ src/puppeteer/index.ts
@@ -5,6 +5,11 @@ import chromium from "chrome-aws-lambda";
 const REQ_TIMEOUT = 8000;
 const IS_LAMBDA = process.env.NODE_ENV === "production";
 
+// @todo – @starthere – Still unable to make scraping with
+// puppeteer/puppeteer-cluster/chronium hosted in vercel
+// This seems to be the most up to date/relevant thread
+// https://gist.github.com/kettanaito/56861aff96e6debc575d522dd03e5725
+
 const puppeteerClient = async () => {
   const launchOptions = {
     concurrency: Cluster.CONCURRENCY_PAGE,
